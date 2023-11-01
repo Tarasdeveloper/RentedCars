bbe8fb670e9bbe5a93b9a209bc6e9d8417fba907
Tarasdeveloper <tomahavki1@gmail.com>
2023-11-01 16:17:26 +0200

    reedme and app update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/RentedCars">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
