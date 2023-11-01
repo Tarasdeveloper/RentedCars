79b411aa8eb61e5cf3008f0b8facc2dda68c6e96
Tarasdeveloper <tomahavki1@gmail.com>
2023-11-01 20:34:33 +0200

    fonts added

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,10 +1,9 @@
 import styled from '@emotion/styled';
-import example from './assets/background.png';
 
 export const AppWrapper = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url(${example});
+  background-image: url();
   background-repeat: no-repeat;
   background-position: center center;
   background-size: contain;
