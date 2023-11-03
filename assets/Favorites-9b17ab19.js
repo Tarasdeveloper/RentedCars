7e4935f2ef64addedede22f7a03cca87aff0ad31
Tarasdeveloper <107717399+Tarasdeveloper@users.noreply.github.com>
2023-11-03 13:19:29 +0000

    Deploying to gh-pages from @ Tarasdeveloper/RentedCars@852b6316c0b646c3cb6d18185fa9d66089ac6f76 🚀

---
+++ assets/Favorites-9b17ab19.js
@@ -0,0 +1,22 @@
+import{s as o,L as e,j as t}from"./index-0696eb9e.js";const s=o.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 500px;
+`,n=o.div`
+  text-align: center;
+`,r=o.h1`
+  color: #000000;
+`;o(e)`
+  padding: 8px 16px;
+  color: #ffffff; /* White text color */
+
+  transition:
+    color 0.3s,
+    text-shadow 0.3s;
+  display: block;
+  &:hover,
+  &:focus {
+    color: #00baff; /* Blue text color on hover/focus */
+  }
+`;const i=()=>t.jsx(s,{children:t.jsx(n,{children:t.jsx(r,{children:"Second Page"})})});export{i as default};
---
+++ assets/HomePage-f8821704.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-0696eb9e.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
