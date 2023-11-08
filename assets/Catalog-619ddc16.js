46019da5af2dc1421d0b0f938004553748d7524e
Tarasdeveloper <107717399+Tarasdeveloper@users.noreply.github.com>
2023-11-08 15:32:17 +0000

    Deploying to gh-pages from @ Tarasdeveloper/RentedCars@bfd3f9077bf6134cfb91ed6cac7c05c7ecb422e9 🚀

--- assets/Catalog-619ddc16.js
+++ assets/Catalog-619ddc16.js
@@ -1,4 +1,4 @@
-import{r as n,s as i,j as r,C as u,u as l,d,e as x}from"./index-e973fd64.js";import{f as m,C,u as f,s as g,a as h,b as j,c as L,F as b}from"./CarItem-b9a0c23b.js";import{P}from"./index-3ea66982.js";const y=(t=10)=>{const[e,a]=n.useState(1),[s]=n.useState(t);return[s,e,()=>a(o=>o+1)]},F=i.button`
+import{r as n,s as i,j as r,C as u,u as l,d,e as x}from"./index-7ecb8f1a.js";import{f as m,C,u as f,s as g,a as h,b as j,c as L,F as b}from"./CarItem-cd48c751.js";import{P}from"./index-4b269f73.js";const y=(t=10)=>{const[e,a]=n.useState(1),[s]=n.useState(t);return[s,e,()=>a(o=>o+1)]},F=i.button`
   color: var(--accent);
   margin: 0 auto;
   margin-bottom: 50px;
