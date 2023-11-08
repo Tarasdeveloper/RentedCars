527811fd46d2e308a54a2cafb8b5322da1e1725e
Tarasdeveloper <tomahavki1@gmail.com>
2023-11-08 15:27:57 +0200

    Update FavoritesList.styled.js

--- src/components/Cars/FavoritesList/FavoritesList.styled.js
+++ src/components/Cars/FavoritesList/FavoritesList.styled.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const FavoreList = styled.ul`
   display: flex;
   flex-wrap: wrap;
+  column-gap: 30px;
+  row-gap: 50px;
+  margin-bottom: 100px;
 `;
