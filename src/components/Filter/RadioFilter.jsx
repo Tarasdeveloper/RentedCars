4f0fe11961d71ddc12cc479b21c50d12219c2d58
Tarasdeveloper <tomahavki1@gmail.com>
2023-11-08 16:39:40 +0200

    Update RadioFilter.jsx

--- src/components/Filter/RadioFilter.jsx
+++ src/components/Filter/RadioFilter.jsx
@@ -7,7 +7,7 @@ import { selectFavorites, selectFilter } from '../../redux/cars/carsSelectors';
 import { setFilter } from '../../redux/cars/carsSlice';
 import { RadioList } from './RadioFilter.styled';
 
-const RadioFilter = ({ open }) => {
+const RadioFilter = () => {
   const dispatch = useDispatch();
   const cars = useSelector(selectFavorites);
   const filter = useSelector(selectFilter);
@@ -23,23 +23,16 @@ const RadioFilter = ({ open }) => {
     }
   };
 
-  const showAllMakes =
-    (window.innerWidth >= 768 && uniqueMakes.length < 2) || open;
-
   return (
     <RadioList>
       {uniqueMakes.map((rentalCompany, index) => (
-        <li
-          key={index}
-          className={`${!showAllMakes && index > 0 ? 'hidden' : ''}`}
-        >
+        <li key={index}>
           <input
             id={rentalCompany}
             type="radio"
             name="make"
             value={rentalCompany}
             checked={filter?.rentalCompany === rentalCompany}
-            className="radio"
             onChange={() => handleMakeChange(rentalCompany)}
           />
           <label htmlFor={rentalCompany}>{rentalCompany}</label>
